737708b709face1fe89b24a033b9bf6b8ab55a20
rainny <jeffreykelechi57@gmail.com>
2024-05-03 13:45:09 +0100

    PHP commit files - corrected

--- app/src/main/java/com/example/onefit/login.php
+++ app/src/main/java/com/example/onefit/login.php
@@ -1,7 +1,7 @@
 <?php
 require "conn.php";
-$user_Name = $_POST["Rainny"];
-$user_pass = $_POST["12345"];
+$user_Name = $_POST["username"];
+$user_pass = $_POST["password"];
 $mysql_query = "select * from user_data where username like '$user_Name' and password like '$user_pass';";
 $mysql_result = mysqli_query($conn, $mysql_query);
 if(mysqli_num_rows($mysql_result) > 0) {
